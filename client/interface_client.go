d09750e6c51ccef0ef89fb6adb0fe9f65eb7c603
wangxiaoming <wangxiaoming@joyy.com>
2023-07-04 17:15:25 +0800

    feat: add client interface

---
+++ client/interface_client.go
@@ -0,0 +1,24 @@
+package client
+
+import (
+	"context"
+	"database/sql"
+)
+
+// DBX database/sql interface
+type DBX interface {
+	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
+	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
+}
+
+// RowsX database/sql Rows interface
+type RowsX interface {
+	Columns() ([]string, error)
+	ColumnTypes() ([]*sql.ColumnType, error)
+	Next() bool
+	Scan(dest ...interface{}) error
+	Err() error
+	Close() error
+}
